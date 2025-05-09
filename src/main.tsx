af96c91021422c821a7ad6befbea39adfd50f7ce
prathamsingh <prathamsingh003@gmail.com>
2025-05-10 00:31:16 +0530

    fixed /

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={client}>
       <AuthProvider>
-        <Router basename="/social-media-vite-supabase">
+        <Router>
           <App />
         </Router>
       </AuthProvider>
